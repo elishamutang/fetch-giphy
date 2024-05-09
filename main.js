bc5d1b381c61200f8af4ff3efb6ab9ae6da2f91b
elishamutang <elishamutang@outlook.com>
2024-05-10 00:21:38 +0800

    complete

--- main.js
+++ main.js
@@ -1,55 +1,28 @@
 ;(() => {
     'use strict'
-    var t = {}
-    ;(t.g = (function () {
-        if ('object' == typeof globalThis) return globalThis
-        try {
-            return this || new Function('return this')()
-        } catch (t) {
-            if ('object' == typeof window) return window
-        }
-    })()),
-        (() => {
-            var e
-            t.g.importScripts && (e = t.g.location + '')
-            var r = t.g.document
-            if (!e && r && (r.currentScript && (e = r.currentScript.src), !e)) {
-                var a = r.getElementsByTagName('script')
-                if (a.length) for (var n = a.length - 1; n > -1 && (!e || !/^http(s?):/.test(e)); ) e = a[n--].src
-            }
-            if (!e) throw new Error('Automatic publicPath is not supported in this browser')
-            ;(e = e
-                .replace(/#.*$/, '')
-                .replace(/\?.*$/, '')
-                .replace(/\/[^\/]+$/, '/')),
-                (t.p = e)
-        })()
-    const e = t.p + 'github-mark.png',
-        r = document.querySelector('form'),
-        a = document.getElementById('gif'),
-        n = document.getElementById('githubProf')
-    ;(n.src = e),
-        (n.src = r.addEventListener('submit', (t) => {
-            t.preventDefault()
-            const e = new FormData(r).get('search-bar')
-            '' !== e &&
-                (async function (t) {
-                    const e = new URL('https://api.giphy.com/v1/gifs/translate')
-                    e.searchParams.append('api_key', 'XKelAPHESxBXFhHXS8Hv8Ou4VzWbEbhy'), e.searchParams.append('s', t)
-                    try {
-                        let t = await fetch(e)
-                        if (!t.ok) throw new Error(`${t.status}`)
-                        {
-                            let e = await t.json()
-                            if ('' === e.meta.response_id) throw new Error('Something wrong with GIPHY systems.')
-                            ;(a.alt = e.data.alt_text),
-                                (a.src = e.data.images.original.webp),
-                                (a.className = 'active'),
-                                console.log(e)
-                        }
-                    } catch (t) {
-                        console.error(t)
+    const e = document.querySelector('form'),
+        t = document.getElementById('gif')
+    e.addEventListener('submit', (a) => {
+        a.preventDefault()
+        const s = new FormData(e).get('search-bar')
+        '' !== s &&
+            (async function (e) {
+                const a = new URL('https://api.giphy.com/v1/gifs/translate')
+                a.searchParams.append('api_key', 'XKelAPHESxBXFhHXS8Hv8Ou4VzWbEbhy'), a.searchParams.append('s', e)
+                try {
+                    let e = await fetch(a)
+                    if (!e.ok) throw new Error(`${e.status}`)
+                    {
+                        let a = await e.json()
+                        if ('' === a.meta.response_id) throw new Error('Something wrong with GIPHY systems.')
+                        ;(t.alt = a.data.alt_text),
+                            (t.src = a.data.images.original.webp),
+                            (t.className = 'active'),
+                            console.log(a)
                     }
-                })(e)
-        }))
+                } catch (e) {
+                    console.error(e)
+                }
+            })(s)
+    })
 })()
