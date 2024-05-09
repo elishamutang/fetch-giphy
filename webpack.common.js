f0fc08b9a64604541462f9563668a1d1c29895b4
elishamutang <elishamutang@outlook.com>
2024-05-10 00:23:40 +0800

    added title.

--- webpack.common.js
+++ webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '',
+            title: 'GIF Generator',
             filename: 'index.html',
             template: 'src/template.html',
         }),
